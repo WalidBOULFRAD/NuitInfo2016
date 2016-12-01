22f328bdd4ab971461aa8c35d137f488de9ea7a5
paul.faidherbe <paul.faidherb@valtech.fr>
2016-12-02 00:29:01 +0100

    Closes #14 Création classe POI

--- backend/src/main/java/com/m2dl/nuitinfo2016/model/poi/POI.java
+++ backend/src/main/java/com/m2dl/nuitinfo2016/model/poi/POI.java
@@ -4,4 +4,10 @@ package com.m2dl.nuitinfo2016.model.poi;
  * Created by Paul.Faidherbe on 01/12/2016.
  */
 public abstract class POI {
+
+    private String name;
+
+    private String address;
+
+    private String description;
 }
